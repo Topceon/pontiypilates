2ae91557d2255fdd73ec3c7b3cd9b79a7cc85e43
Inox Danil <inoxdanil@gmail.com>
2023-02-22 15:02:11 +0600

    js обращается к php, запись в бд срабатывает

--- scripts.js
+++ scripts.js
@@ -9,12 +9,10 @@ function query_php() {
     let xhttp = new XMLHttpRequest()
     xhttp.onload = function () {
         if (this.response !== '0'){
-            for (let i = 0; i < 17; i++) {
             console.log('test')
-            }
         }
     }
-    xhttp.open("GET", 'http://' + SITE_NAME + '/groups.php');
+    xhttp.open("GET", 'http://pontiypilates/groups.php');
     xhttp.send();
 }
 
@@ -25,17 +23,7 @@ query_php()
 
 
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'groups.php', true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log("User created successfully");
-    } else if (xhr.status !== 200) {
-        console.log("Error: " + xhr.responseText);
-    }
-};
-xhr.send("username=newuser&password=password123");
+
 
 // функция достающая все группы из БД
 
