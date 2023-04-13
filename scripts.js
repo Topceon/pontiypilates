11e667d3fba3d92e4418990a5b55c7b5f07e9341
Inox Danil <inoxdanil@gmail.com>
2023-04-13 10:05:18 +0600

    рефакторинг переключения страниц

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ SITE_NAME = 'pontiypilates'
 // получаем текущую дату
 let currentDate = new Date().toISOString().substr(0, 10);
 let group_id_for_php = 0;
+let customers = []
 
 
 // переменная для переключения между стандартными страничками
@@ -15,7 +16,7 @@ function create_new_db() {
     let newdb_btn = document.getElementById('newdb')
     let xhttp = new XMLHttpRequest()
     xhttp.onload = function () {
-        if (this.response === 'База данных успешно созданаТаблица создана успешно'){
+        if (this.response === 'База данных успешно созданаТаблица создана успешно') {
             //кнопка должна быть удалена
             newdb_btn.remove();
             console.log(this.response)
@@ -29,13 +30,13 @@ function create_new_db() {
 function query_check_db() {
     let xhttp = new XMLHttpRequest()
     xhttp.onload = function () {
-        if (this.response === 'База данных подключена'){
+        if (this.response === 'База данных подключена') {
             all_groups_from_db()
             console.log(this.response)
             // устанавливаем текущую дату в датаинпут
             let date_attendance = document.getElementById('date_attendance');
             date_attendance.value = currentDate;
-        }else {
+        } else {
             let add_div_var = document.getElementById(`perva`)
             add_div_var.insertAdjacentHTML('afterend', `<div class='row' id='newdb'>Создать новую базу данных?</div>`)
             console.log('Создать новую базу данных?')
@@ -49,7 +50,7 @@ function query_check_db() {
 query_check_db()
 
 
-function create_ready_btn(foo){
+function create_ready_btn(foo) {
     let parent_for_clear = document.getElementById('customersList');
     parent_for_clear.innerHTML = '';
     const ready_btn = document.createElement('div');
@@ -61,34 +62,54 @@ function create_ready_btn(foo){
 }
 
 
-function customers_by_group(group_id_for_php){
+function switch_page_foo() {
+    if (switch_page === "attendance_page") {
+        attendance_page_foo()
+    } else if (switch_page === "payment_page") {
+        payment_page_foo()
+    }
+}
+
+
+function attendance_page_foo() {
+    if (customers.length > 0) {
+        create_ready_btn(attendance_insert_to_db)
+        for (let i = 0; i < customers.length; i++) {
+            let customer = customers[i];
+            let add_div_var = document.getElementById(`perva`)
+            add_div_var.insertAdjacentHTML('beforebegin', `<div class='row' id=${customer['customer_id']}>${customer['customer_name']}
+            <input class="ch_box" name="${customer['customer_id']}" type="checkbox">
+            </div>`)
+        }
+    } else {
+        let parent_for_clear = document.getElementById('customersList');
+        parent_for_clear.innerHTML = '';
+    }
+}
+
+
+function payment_page_foo() {
+    if (customers.length > 0) {
+        create_ready_btn(payment_insert_to_db)
+        for (let i = 0; i < customers.length; i++) {
+            let customer = customers[i];
+            let add_div_var = document.getElementById(`perva`)
+            add_div_var.insertAdjacentHTML('beforebegin', `<div class='row' id=${customer['customer_id']}>${customer['customer_name']}
+            <input class="payment_input" name="${customer['customer_id']}" type="text">
+            </div>`)
+        }
+    } else {
+        let parent_for_clear = document.getElementById('customersList');
+        parent_for_clear.innerHTML = '';
+    }
+}
+
+
+function customers_by_group() {
     let xhttp = new XMLHttpRequest()
     xhttp.onload = function () {
         customers = JSON.parse(this.response)
-        if (customers.length > 0) {
-            if (switch_page === "attendance_page") {
-                create_ready_btn(attendance_insert_to_db)
-                for (let i = 0; i < customers.length; i++) {
-                    let customer = customers[i];
-                    let add_div_var = document.getElementById(`perva`)
-                    add_div_var.insertAdjacentHTML('beforebegin', `<div class='row' id=${customer['customer_id']}>${customer['customer_name']}
-                    <input class="ch_box" name="${customer['customer_id']}" type="checkbox">
-                    </div>`)
-                }
-            } else if (switch_page === "payment_page") {
-                create_ready_btn(payment_insert_to_db)
-                for (let i = 0; i < customers.length; i++) {
-                    let customer = customers[i];
-                    let add_div_var = document.getElementById(`perva`)
-                    add_div_var.insertAdjacentHTML('beforebegin', `<div class='row' id=${customer['customer_id']}>${customer['customer_name']}
-                    <input class="payment_input" name="${customer['customer_id']}" type="text">
-                    </div>`)
-                }
-            }
-        }else {
-            let parent_for_clear = document.getElementById('customersList');
-            parent_for_clear.innerHTML = '';
-        }
+        switch_page_foo()
     }
     xhttp.open("POST", 'http://' + SITE_NAME + '/customers.php', true);
     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -107,9 +128,9 @@ function all_groups_from_db() {
             option.text = groups[i].group;
             select.add(option);
         }
-        select.addEventListener('change', (event) =>{
+        select.addEventListener('change', (event) => {
             group_id_for_php = event.target.value;
-            customers_by_group(group_id_for_php);
+            customers_by_group();
         })
     }
     xhttp.open("GET", 'http://' + SITE_NAME + '/groups.php');
@@ -127,7 +148,7 @@ function attendance_insert_to_db() {
         check_comma = 0
         for (let i = 0; i < elements_to_attendance.length; i++) {
             if (elements_to_attendance[i].checked) {
-                if (check_comma){
+                if (check_comma) {
                     customer_data = customer_data + ",";
                 }
                 check_comma += 1;
@@ -154,7 +175,7 @@ function payment_insert_to_db() {
         check_comma = 0
         for (let i = 0; i < elements_to_payment.length; i++) {
             if (elements_to_payment[i].value !== '') {
-                if (check_comma){
+                if (check_comma) {
                     customer_data = customer_data + ",";
                 }
                 check_comma += 1;
@@ -173,14 +194,18 @@ function payment_insert_to_db() {
 }
 
 
-function switch_page_btns(btn_id){
+function switch_page_btns(btn_id) {
     switch_page = btn_id.target.id
-    customers_by_group(group_id_for_php);
+    // customers_by_group(group_id_for_php);
+    switch_page_foo()
 }
+
 let foot_btn = document.getElementsByClassName('foot_btn')
 for (let i = 0; i < foot_btn.length; i++) {
     foot_btn[i].onclick = switch_page_btns
 }
+
+
 // функция срабатывающая при нажатии на кнопку создания нового клиента
 
 
